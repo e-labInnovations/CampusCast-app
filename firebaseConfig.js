97ac098cd07ffb128bbbb25e96bdeb765726b448
e-labInnovations <mohammedashad23@gmail.com>
2023-04-06 15:00:40 +0530

    login page started

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXTYvGDuBx17zE_KEMnhPxcKzA-jkSrAI",
+  authDomain: "campuscast-elabins.firebaseapp.com",
+  projectId: "campuscast-elabins",
+  storageBucket: "campuscast-elabins.appspot.com",
+  messagingSenderId: "227472355966",
+  appId: "1:227472355966:web:7c9eab5843f27476665e2f",
+  measurementId: "G-TZZ3CKM2ZM"
+};
+
+const app = initializeApp(firebaseConfig);
